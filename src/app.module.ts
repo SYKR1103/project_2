418bed1c5f3d39ff7abccd01d17f46922ced25df
sun <thebthebtheb1234@gmail.com>
2023-11-16 23:37:30 +0900

    db port connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { DblistModule } from './dblist/dblist.module';
+import { ConfigModule } from '@nestjs/config/dist';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [],
+  imports: [ProductModule, DblistModule, ConfigModule.forRoot({
+
+    validationSchema : Joi.object({
+
+      POSTGRES_HOST : Joi.string().required(),
+      POSTGRES_PORT : Joi.string().required(),
+      POSTGRES_USER : Joi.string().required(),
+      POSTGRES_PASSWORD : Joi.string().required(),
+      POSTGRES_DB : Joi.string().required(),
+
+
+
+    })
+
+
+
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
